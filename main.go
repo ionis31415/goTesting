5686f408a3015a6785c523fb95927735a8f77b7e
ionis walanchunas <ionis31415@gmail.com>
2023-06-21 22:11:58 -0700

    added makeMult function

--- main.go
+++ main.go
@@ -4,6 +4,25 @@ import (
 	"fmt"
 )
 
+func calculateDaysBetweenDates(date1, date2 string) int {
+
+	return 0
+}
+
+func makeMult(base int) func(int) int {
+	return func(factor int) int {
+		return base * factor
+	}
+}
+
 func main() {
 	fmt.Println("start")
+
+	twoBase := makeMult(2)
+	threeBase := makeMult(3)
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(twoBase(i), threeBase(i))
+	}
+
 }
