76f25de2d5345c0cdf0bd83b602241d85c454856
ionis walanchunas <ionis31415@gmail.com>
2023-06-25 23:17:29 -0700

    added update fn

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ func calculateDaysBetweenDates(date1, date2 string) int {
 	return 0
 }
 
+func update(px *int) {
+	*px = 100
+}
+
 func makeMult(base int) func(int) int {
 	return func(factor int) int {
 		return base * factor * factor
@@ -16,6 +20,9 @@ func makeMult(base int) func(int) int {
 }
 
 func main() {
+
+	x := 10
+
 	fmt.Println("start")
 
 	twoBase := makeMult(2)
@@ -24,5 +31,7 @@ func main() {
 	for i := 0; i < 3; i++ {
 		fmt.Println(twoBase(i), threeBase(i))
 	}
+	update(&x)
+	fmt.Println(x)
 
 }
